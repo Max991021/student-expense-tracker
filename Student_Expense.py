e1ca6a9081edd10b04dd3e25cfbdec6e94a26417
Max991021 <leshopep@gmail.com>
2026-02-17 21:42:48 +0200

    data handling

--- Student_Expense.py
+++ Student_Expense.py
@@ -1,3 +1,4 @@
+import csv
 class Tracker:
     
     def __init__(self,item = None,price = None):
@@ -6,8 +7,20 @@ class Tracker:
     
     def items(self):
         with open('expenses.csv', 'r', errors='ignore') as file:
-            file.read()
+            content = file.read.split(',')
+            
+            names = content[0]
+            expense_catagory = []
+            price = []
+            for i in range(1,len(content),2):
+                expense_catagory.append(content[i])
+            
+            for i in range(2,len(content),2):
+                price.append(content[i])
+                
+            
+                
     
     def amount(self):
-        with open('expenses.csv', 'r', errors='ignore') as file:
+        with open('expense_tracker.csv', 'w', errors='ignore') as file:
             file.read()
