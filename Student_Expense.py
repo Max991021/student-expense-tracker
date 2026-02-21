55eed206f614cceb1f66b36986f976006a5f686f
Max991021 <leshopep@gmail.com>
2026-02-21 18:56:50 +0200

    writing to the file for the sorted expenses

--- Student_Expense.py
+++ Student_Expense.py
@@ -1,38 +1,37 @@
 import csv
 
-class Tracker:
-    
-    def __init__(self,item = None,price = None):
-        self.item = item
-        self.price = price
         
         
+tracker_dict = {}   
+def items():
     
-    def items(self):
-        tracker_dict = {}
-        with open('expenses.csv', 'r', errors='ignore') as file:
-            content = file.read.split(',')
+    with open('expenses.csv', 'r', errors='ignore') as file:
+        content = file.read.split(',')
+        
+        names = content[0]
+        expense_catagory = []
+        price = []
+        for i in range(1,len(content),2):
+            expense_catagory.append(content[i])
+        
+        for i in range(2,len(content),2):
+            price.append(content[i])
             
-            names = content[0]
-            expense_catagory = []
-            price = []
-            for i in range(1,len(content),2):
-                expense_catagory.append(content[i])
+        for i in names:
+            tracker_dict[i] = f'{expense_catagory[i]} R {price}'
+        
+        
+    for i in tracker_dict:
+        tracker_dict[key] = f"Total is R {sum(value)}"
+        
+    tracker_dict = sorted(tracker_dict, value[-1], reverse=False)
             
-            for i in range(2,len(content),2):
-                price.append(content[i])
-                
-            for i in names:
-                tracker_dict[i] = f'{expense_catagory[i]} R {price}'
-            
-           
-        for i in tracker_dict:
-            tracker_dict[key] = f"Total is R {sum(value)}"
-            
-        tracker_dict = sorted(tracker_dict, value[-1], reverse=False)
-                
+    for key, value in tracker_dict.items():
+        print(f'{key}, {value}')
+        
+    return tracker_dict
+def amount():
+    with open('expense_tracker.csv', 'w', errors='ignore') as file:
+        file.read()
         for key, value in tracker_dict.items():
-            print(f'{key}, {value}')
-    def amount(self):
-        with open('expense_tracker.csv', 'w', errors='ignore') as file:
-            file.read()
+            file.write(f'{key}, {value}')
